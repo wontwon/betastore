a76427f387fbf29e57bab253e5568a87a33c4ab0
Tuan Duong <tuanduong15@gmail.com>
2013-10-08 15:46:59 -0400

    added routes for returns

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Betastore::Application.routes.draw do
-  resources :products, :subscriptions
-  root :to => 'products#index'
+	resources :products, :orders
 
+	resources :subscriptions, only:[:create, :new, :show]
 
-  get '/log_in' => 'log_ins#new', as: 'log_in'
-  post '/log_in' => 'log_ins#create'
-  post '/log_out' => 'log_ins#destroy', as: 'log_out'
+	resources :orders do
+		resources :refunds
+	end
+
+	get '/log_in' => 'log_ins#new', as: 'log_in'
+	post '/log_in' => 'log_ins#create'
+	post '/log_out' => 'log_ins#destroy', as: 'log_out'
+
+	root :to => 'products#index'
+
 
 end
