d35de78873b18933b3019f0429c7e0ff2a552ffa
Tuan Duong <tuanduong15@gmail.com>
2013-10-08 15:53:24 -0400

    modified schema after adding return model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131001224654) do
+ActiveRecord::Schema.define(version: 20131008194032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,11 @@ ActiveRecord::Schema.define(version: 20131001224654) do
     t.string   "name"
     t.decimal  "price"
     t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "returns", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
