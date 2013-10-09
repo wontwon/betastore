6e21c5a6b6f362b2978a5b776f8945c465748128
Tuan Duong <tuanduong15@gmail.com>
2013-10-09 15:00:05 -0400

    scheme updated with migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131008194032) do
+ActiveRecord::Schema.define(version: 20131009004235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20131008194032) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password"
   end
 
   create_table "line_items", force: true do |t|
