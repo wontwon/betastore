86b6a70a4aa8a3fb4333bd11cd5f92a848721fda
Tuan Duong <tuanduong15@gmail.com>
2013-10-09 16:52:47 -0400

    used raw to return an HTMl string and have it rendered and not have it interpreted as a string

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,17 @@
 module ApplicationHelper
 
-  def display_name_id(product)
+	def display_name_id(product)
     # raw allows you to return a html string and have it rendered and not have it interperted as a just a "string"
     raw("<div class='box-text'>PID: #{product.id} Name: #{product.name} </div>")
-  end
-  # your old helper
-	# def display_name_id(name, id)
-	# 	p "#{name} - ##{id}"
+end
 
-	# end
 
 # implement these two methods
-  def display_edit_link(product)
-  end
+def display_edit_link(product)
+	link_to "Edit", edit_product_path(product)
+end
 
-  def display_delete_link(product)
-  end
+def display_delete_link(product)
+	link_to "Delete", product, :method => :delete
+end
 end
