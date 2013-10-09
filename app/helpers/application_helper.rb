b37c33951ee6eeb2cf8827e1862d31cfb912f3fb
Stephen Nguyen <nguyenhstephen@gmail.com>
2013-10-09 13:33:43 -0700

    modified view helper added two unimplemented view helpers to do

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,19 @@
 module ApplicationHelper
-	def display_name_id(name, id)
-		p "#{name} - ##{id}"
 
-	end
+  def display_name_id(product)
+    # raw allows you to return a html string and have it rendered and not have it interperted as a just a "string"
+    raw("<div class='box-text'>PID: #{product.id} Name: #{product.name} </div>")
+  end
+  # your old helper
+	# def display_name_id(name, id)
+	# 	p "#{name} - ##{id}"
+
+	# end
+
+# implement these two methods
+  def display_edit_link(product)
+  end
+
+  def display_delete_link(product)
+  end
 end
