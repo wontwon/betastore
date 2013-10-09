d43bf11dbee71baf9c3af6f32af7dd251a06c56f
Tuan Duong <tuanduong15@gmail.com>
2013-10-09 14:58:45 -0400

    added routes for sign up

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Betastore::Application.routes.draw do
 		resources :refunds
 	end
 
+	get '/sign_up' => 'customers#new', as: 'sign_up'
+	post '/sign_up' => 'customers#create'
+
 	get '/log_in' => 'log_ins#new', as: 'log_in'
 	post '/log_in' => 'log_ins#create'
 	post '/log_out' => 'log_ins#destroy', as: 'log_out'
