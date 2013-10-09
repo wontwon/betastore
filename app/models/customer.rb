201d997042cc1ca17346df6b1005af3c8116d8e6
Tuan Duong <tuanduong15@gmail.com>
2013-10-09 14:55:57 -0400

    added relations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,8 @@
 class Customer < ActiveRecord::Base
+	has_many :orders
+	has_many :line_items, :through => :orders
+	has_many :products, :through => :line_items
+
+	validates_presence_of :name, :email, :password
+
 end
