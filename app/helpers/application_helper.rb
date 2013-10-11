375564bfd9a3eb9af16ab161c4b225896b0bcc89
Tuan Duong <tuanduong15@gmail.com>
2013-10-10 22:47:31 -0400

    created links for Edit and Delete to pull into view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,16 @@
 module ApplicationHelper
 
 	def display_name_id(product)
-    # raw allows you to return a html string and have it rendered and not have it interperted as a just a "string"
     raw("<div class='box-text'>PID: #{product.id} Name: #{product.name} </div>")
-end
+	end
 
+	# implement these two methods
+	def display_edit_link(product)
+		link_to "Edit", edit_product_path(product)
+	end
 
-# implement these two methods
-def display_edit_link(product)
-	link_to "Edit", edit_product_path(product)
-end
-
-def display_delete_link(product)
-	link_to "Delete", product, :method => :delete
-end
+	def display_delete_link(product)
+		link_to "Delete", product, :method => :delete
+	end
+	
 end
