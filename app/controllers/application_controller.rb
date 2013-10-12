22e4a9f10a8adad6dc8f1b91940aa3ba6e42ed31
Tuan Duong <tuanduong15@gmail.com>
2013-10-11 21:08:47 -0400

    added signed cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   end
 
   def logged_in?
-    cookies[:email].present?
+    cookies.signed[:customer_id].present?
   end
 
   # Prevent CSRF attacks by raising an exception.
