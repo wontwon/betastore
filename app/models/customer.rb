cc588b6dfea14ee2db2edd7758c8de7efb3dc47a
Tuan Duong <tuanduong15@gmail.com>
2013-10-11 21:10:42 -0400

    added has_secure_password

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,6 +3,9 @@ class Customer < ActiveRecord::Base
 	has_many :line_items, :through => :orders
 	has_many :products, :through => :line_items
 
-	validates_presence_of :name, :email, :password
 
+	validates_presence_of :name, :email
+
+
+	has_secure_password
 end
