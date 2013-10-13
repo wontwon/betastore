09186f0333c1660bd1f98fc00845007f3f4e50a9
Stephen Nguyen <nguyenhstephen@gmail.com>
2013-10-13 02:55:23 -0700

    notes on the login controller. good job overall.

--- app/controllers/log_ins_controller.rb
+++ app/controllers/log_ins_controller.rb
@@ -5,15 +5,22 @@ class LogInsController < ApplicationController
 		customer = Customer.find_by(email: params[:email])
 		if customer && customer.authenticate(params[:password_digest])
 			cookies.signed[:customer_id] = customer.id
-			redirect_to products_path
+			redirect_to(products_path)
 		else
-			redirect_to log_in_path, alert: 'Log In Failed'
+			# in the alert be more expressive about "WHY" login failed
+			# hint customer.errors.full_messages
+			redirect_to(log_in_path, alert: 'Log In Failed')
+			# minor note try to either choose to use () when you are calling methods or don't
+			# consistincy with this is import
+			# redirect_to(log_in_path, alert: 'Log In Failed')
+			# redirect_to log_in_path, alert: 'Log In Failed'
+			# very least make sure you keep consistancy within the same controller action
 		end
 	end
 
 	def destroy
 		cookies.delete(:email)
-		redirect_to products_path		
+		redirect_to(products_path)		
 	end
 
 
