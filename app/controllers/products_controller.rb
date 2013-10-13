6caa9d81f4245267f2b92500647381de788f0424
Stephen Nguyen <nguyenhstephen@gmail.com>
2013-10-13 02:48:01 -0700

    refactored your products controller to be more DRY. added comments

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,12 @@
 class ProductsController < ApplicationController
 	 #before_filter :require_log_in
+	 
+	 # see the set product method and think about what this "callback" before_action just did
+	 before_action :set_product, only: [:show, :edit, :destroy]
+
 
 	def show
-		@product = Product.find(params[:id])
+		# @product = Product.find(params[:id])
 	end
 
 	def index
@@ -10,13 +14,19 @@ class ProductsController < ApplicationController
 	end
 
 	def edit
-		@product = Product.find(params[:id])
+		# @product = Product.find(params[:id])
 	end
 
 	def destroy
-		@product = Product.find(params[:id])
+		# @product = Product.find(params[:id])
 		@product.destroy
 		redirect_to products_path
 	end
 
+
+  private
+    # Use callbacks to share common setup methods like finding the product
+    def set_product
+      @product = Product.find(params[:id])
+    end
 end
