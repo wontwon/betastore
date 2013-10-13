1790002082086514f3aecce1f91ee60df5d97ac7
Stephen Nguyen <nguyenhstephen@gmail.com>
2013-10-13 03:11:40 -0700

    This is how I would execute your seed file. try it out rake db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# useless_variable_assignment = Product.create(name: 'Shirt', price: 2.99, url: 'http://pjb3.github.io/betastore/products/shirt.jpg')
+# useless_variable_assignment = Product.create(name: 'IPhone', price: 3.75, url: 'http://pjb3.github.io/betastore/products/iphone_case.jpg')
+# useless_variable_assignment = Product.create(name: 'Hoodie', price: 3.99, url: 'http://pjb3.github.io/betastore/products/hoodie.jpg')
+# useless_variable_assignment = Product.create(name: 'Journal', price: 3.99, url: 'http://pjb3.github.io/betastore/products/journal.jpg')
+# useless_variable_assignment = Product.create(name: 'Sticker', price: 3.99, url: 'http://pjb3.github.io/betastore/products/sticker.jpg')
+# useless_variable_assignment = Product.create(name: 'Hat', price: 3.99, url: 'http://pjb3.github.io/betastore/products/hat.jpg')
 
 
-Shirt = Product.create(name: 'Shirt', price: 2.99, url: 'http://pjb3.github.io/betastore/products/shirt.jpg')
-IPhone = Product.create(name: 'IPhone', price: 3.75, url: 'http://pjb3.github.io/betastore/products/iphone_case.jpg')
-Hoodie = Product.create(name: 'Hoodie', price: 3.99, url: 'http://pjb3.github.io/betastore/products/hoodie.jpg')
-Journal = Product.create(name: 'Journal', price: 3.99, url: 'http://pjb3.github.io/betastore/products/journal.jpg')
-Sticker = Product.create(name: 'Sticker', price: 3.99, url: 'http://pjb3.github.io/betastore/products/sticker.jpg')
-Hat = Product.create(name: 'Hat', price: 3.99, url: 'http://pjb3.github.io/betastore/products/hat.jpg')
+# useless_variable_assignment = Customer.create(name: 'Tuan', email: 'tuan@tuan.com', password:'secret')
+# useless_variable_assignment = Customer.create(name: 'Stephen', email: 'Stephen@stephen.com', password:'secret')
+# useless_variable_assignment = Customer.create(name: 'Tony', email: 'Tony@tony.com', password:'tony')
+# useless_variable_assignment = Customer.create(name: 'Rob', email: 'Rob@rob.com', password:'secret')
 
+# There is no use to assigning each customer to variables you are not doing anything with them, I renamed to useless_variable_assignment
 
-Tuan = Customer.create(name: 'Tuan', email: 'tuan@tuan.com', password:'secret')
-Tuan = Customer.create(name: 'Stephen', email: 'Stephen@stephen.com', password:'secret')
-Tuan = Customer.create(name: 'Tony', email: 'Tony@tony.com', password:'tony')
-Tuan = Customer.create(name: 'Rob', email: 'Rob@rob.com', password:'secret')
+ARRAY_OF_IMGS = ['http://pjb3.github.io/betastore/products/shirt.jpg', 'http://pjb3.github.io/betastore/products/iphone_case.jpg', 'http://pjb3.github.io/betastore/products/hoodie.jpg', 'http://pjb3.github.io/betastore/products/journal.jpg', 'http://pjb3.github.io/betastore/products/sticker.jpg', 'http://pjb3.github.io/betastore/products/hat.jpg']
+
+400.times do |x|
+Product.create(name: Faker::Commerce.product_name, price: rand(0.99...9999.0).round(2), url: ARRAY_OF_IMGS.sample)
+p "product created"
+end
+  
+
+200.times do |x|
+Customer.create(name: Faker::Name.name, email: Faker::Internet.email, password:'secret')
+p "customer created"
+end
